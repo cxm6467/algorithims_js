d51ee6cac53d0b207ccf49fff29f90fe66c5b49a
Caboose <cxm6467@gmail.com>
2021-10-03 05:09:44 -0400

    Added function to get product of array except current index

--- algo_practice.js
+++ algo_practice.js
@@ -274,3 +274,31 @@ function sortLinkedList(list) {
     i = i.next;
   }
 }
+
+/**
+ * Get product of array except current index
+ * 
+ * @param {int []} - Int array of values to check
+ * @returns {int []} - Int [] with products
+ */
+
+ function productExceptSelf(arr){
+  let n = arr.length;
+  let cumulativeFromLeft = [...Array(n)].map(x => 0);
+  cumulativeFromLeft[0] = 1;
+  for(let i = 1; i < n; i++)
+    cumulativeFromLeft[i] = cumulativeFromLeft[i-1] * arr[i-1];
+
+  let cumulativeFromRight = [...Array(n)].map(x => 0);
+  cumulativeFromRight[n-1] = 1;
+  for(let i = n-2; i >= 0; i--)
+    cumulativeFromRight[i] = cumulativeFromRight[i+1] * arr[i+1];
+
+  let output = [...Array(n)].map(x => 0);
+  for(let i = 0; i < n; i++)
+    output[i] = cumulativeFromLeft[i] * cumulativeFromRight[i];
+  
+  return output;
+}
+
+
